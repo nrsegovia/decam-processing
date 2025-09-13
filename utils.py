a9304ac140bf2eab53f0c8cdd87db207945edb03
Nicolas Rodriguez-Segovia <nj.rsegovia@gmail.com>
2025-09-13 18:13:06 -0300

    Problem in get_fron_header function fixed

--- utils.py
+++ utils.py
@@ -52,7 +52,7 @@ def list_directories_no_band(main_dir, ccds, single_ccd):
 
 def get_from_header(file_path: Path, column: str):
     header = pd.read_csv(Path(file_path.parent, "Header.info"))
-    row = header.File.values[header.File.values == file_path.name].index.to_list()[0]
+    row = header.File[header.File.values == file_path.name].index.to_list()[0]
     try:
         value = header.at[row, column]
     except Exception as e:
